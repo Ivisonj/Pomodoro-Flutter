d779660730988ea21a3e664f79b0348230e013c1
ivisonj <ivison_j@outlook.com>
2025-08-13 12:27:56 -0300

    feat: implement Stopwatch component

--- lib/pages/Pomodoro.dart
+++ lib/pages/Pomodoro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/components/EntryTime.dart';
+import 'package:pomodoro/components/Stopwatch.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
@@ -8,14 +9,18 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntryTime(title: 'Trabalho', value: 25),
-              EntryTime(title: 'Descanso', value: 5),
-            ],
+          Expanded(child: Stopwatch()),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntryTime(title: 'Trabalho', value: 25),
+                EntryTime(title: 'Descanso', value: 5),
+              ],
+            ),
           ),
         ],
       ),
